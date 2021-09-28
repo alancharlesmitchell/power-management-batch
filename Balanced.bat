9badefb9f709c59d7beda5a897d9784c3f3bc905
alancharlesmitchell <91332410+alancharlesmitchell@users.noreply.github.com>
2021-09-28 12:56:25 -0500

    added balanced, ultimate performance batch scripts

---
+++ Balanced.bat
@@ -0,0 +1,3 @@
+@echo off
+powercfg -s 381b4222-f694-41f0-9685-ff5bb260df2e
+powercfg /setdcvalueindex scheme_current sub_energysaver esbattthreshold 100
---
+++ Ultimate Performance.bat
@@ -0,0 +1,3 @@
+@echo off
+powercfg -s 74132d54-3cd1-4ee2-b695-b100fe766998
+powercfg /setdcvalueindex scheme_current sub_energysaver esbattthreshold 0
